8fa87f1335d264cbb0673280ce8b5813138af04f
Rodoca <caceresrodolfoh@gmail.com>
2022-05-29 17:49:40 -0300

    10,11,12,13,14 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -39,36 +39,32 @@ function resta(x, y) {
 }
 
 function multiplica(x, y) {
-  // Multiplica "x" por "y" y devuelve el valor
-  // Tu código:
-  
+     return (x * y);
 }
 
 function divide(x, y) {
-  // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
-  
+     return (x / y);
 }
 
 function sonIguales(x, y) {
-  // Devuelve "true" si "x" e "y" son iguales
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+     if (x === y)
+     return true
+     else 
+     return false
 }
 
 function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+     if (str1.length === str2.length)
+     return true
+     else
+     return false
 }
 
 function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+     if (num < 90)
+     return true
+     else
+     return false
 }
 
 function mayorQueCincuenta(num) {
