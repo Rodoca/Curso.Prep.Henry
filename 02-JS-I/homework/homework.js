0d951558470c4292f8e4a77c81b8f8561ea9717c
Rodoca <caceresrodolfoh@gmail.com>
2022-05-29 16:17:49 -0300

    seventh and eighth commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -26,16 +26,11 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  // "Return" la string provista: str
-  // Tu código:
-  
+     return str;
 }
 
 function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
-  
+     return (x + y); 
 }
 
 function resta(x, y) {
