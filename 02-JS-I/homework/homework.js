667c0757ef38f5dc45070a6245486abe0c0d7ac8
Rodoca <caceresrodolfoh@gmail.com>
2022-05-30 09:56:32 -0300

    21,22,23,24,25 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -101,27 +101,19 @@ function elevarAlCubo(num) {
 }
 
 function elevar(num, exponent) {
-  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:
-  
+     return Math.pow(num, exponent) 
 }
 
 function redondearNumero(num) {
-  // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
-  
+     return Math.round( num )
 }
 
 function redondearHaciaArriba(num) {
-  // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
-  // Tu código:
-  
+     return Math.ceil( num ) 
 }
 
 function numeroRandom() {
-  //Generar un número al azar entre 0 y 1 y devolverlo
-  //Pista: investigá qué hace el método Math.random()
-  
+     return Math.random();
 }
 
 function esPositivo(numero) {
@@ -129,7 +121,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+     if (numero === 0)
+     return false
+     if (numero > 0)
+     return "Es positivo"
+     if (numero < 0)
+     return "Es negativo"
 }
 
 function agregarSimboloExclamacion(str) {
